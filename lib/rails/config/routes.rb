73003018cac49b0bc79d89805a1dd7d7a8be72a4
Sun Yun-Rui <swingsfcicada@gmail.com>
2016-03-16 16:20:32 +0800

    Update routes.rb
    change default path to load js

--- lib/rails/config/routes.rb
+++ lib/rails/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   if Rails.version >= '4.0.0'
-    match "/websocket", :to => WebsocketRails::ConnectionManager.new, via: [:get, :post]
+    match "/websocket/io", :to => WebsocketRails::ConnectionManager.new, via: [:get, :post]
   else
-    match "/websocket", :to => WebsocketRails::ConnectionManager.new
+    match "/websocket/io", :to => WebsocketRails::ConnectionManager.new
   end
 end
